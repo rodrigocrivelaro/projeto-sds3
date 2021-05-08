d210daeb5f3d88fd61fa4f5af16511d99b5a48bb
Rodrigo Crivelaro dos Reis <rreiscrivelaro@gmail.com>
2021-05-08 16:22:22 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -22,11 +22,6 @@ const DonutChart = () => {
             });
     }, []);
 
-    // const mockData = {
-    //     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé'],
-    //     series: [477138, 499928, 444867, 220426, 473088]
-    // }
-
     const options = {
         legend: {
             show: true
